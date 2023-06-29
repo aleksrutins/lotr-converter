38cd9371f9a202f86ac4a9231b80159c9d652cd8
Aleks Rutins <aleks@rutins.com>
2023-06-29 17:11:27 -0400

    Initial Commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by Aleks Rutins on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
